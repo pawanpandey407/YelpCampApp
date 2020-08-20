49cce67e5395e179005deace17cebfc6edecc4c5
Pawan Pandey <pawanpandey407@gmail.com>
2020-08-20 17:42:54 -0500

    Connect to mongodb cloud

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var campgroundRoutes = require("./routes/campgrounds"),
            indexRoutes = require("./routes/index")
  
 mongoose
-      .connect("mongodb://localhost:27017/yelp_camp_v12", {
+      .connect("mongodb+srv://pandey_pawan:awesome143@cluster0.gevxx.mongodb.net/<dbname>?retryWrites=true&w=majority", {
           useNewUrlParser: true,
           useUnifiedTopology: true
       })
