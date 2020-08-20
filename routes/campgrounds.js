391b6af280038e0e4037525c2395b4523b1e2995
Pawan Pandey <pawanpandey407@gmail.com>
2020-08-20 18:40:16 -0500

    add console.log err

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -39,6 +39,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
   geocoder.geocode(req.body.location, function (err, data) {
     if (err || !data.length) {
       req.flash("error", "Invalid address");
+      console.log(err);
       return res.redirect('back');
     }
     var lat = data[0].latitude;
